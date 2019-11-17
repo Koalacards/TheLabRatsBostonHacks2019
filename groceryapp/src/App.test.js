7e366b8a6069bdc0b50d23bb2536760172d5781a
Ronja <38873366+Rebranding@users.noreply.github.com>
2019-11-17 05:39:00 -0500

    not much

--- groceryapp/src/App.test.js
+++ groceryapp/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Result';
+import Fullscreen from './fullscreen';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Fullscreen />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
