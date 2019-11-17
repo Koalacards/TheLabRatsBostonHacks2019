5e8cfe772ba26bd07a17f752a6166c51fca3d354
Dania-Abuhijleh <57302458+Dania-Abuhijleh@users.noreply.github.com>
2019-11-16 20:12:26 -0500

    buggy

--- groceryapp/src/App.js
+++ groceryapp/src/App.js
@@ -1,32 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
 import './App.css';
+// import "@material/top-app-bar/mdc-top-app-bar";
+// import "@material/icon-button/mdc-icon-button";
 
-function App() {
-  return (
-    <div className="Groceries!">
-      <header className="Find some groceries!" >
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Search for what groceries you want! This website will tell you what the best prices are around!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
-        <br>
-        </br>
-        <input type="text" placeholder="Search for groceries here!">
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
 
-        </input>
-
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    )
+  }
 }
+export default App
 
-export default App;
+// function App() {
+//   return (
+    //<div className="Groceries!">
+      // <header className="Find some groceries!" >
+      //   <img src={logo} className="App-logo" alt="logo" />
+      //   <p>
+      //     Search for what groceries you want! This website will tell you what the best prices are around!
+      //   </p>
+      //   <a
+      //     className="App-link"
+      //     href="https://www.google.com"
+      //     target="_blank"
+      //     rel="noopener noreferrer"
+      //   >
+      //     Test
+      //   </a>
+      //   <br>
+      //   </br>
+      //   <input type="text" placeholder="Search for groceries here!">
+      //
+      //   </input>
+      //
+      // </header>
+// <div>
+// <head>
+//   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+// </head>
+//
+// <header class="mdc-top-app-bar">
+//   <div class="mdc-top-app-bar__row">
+//     <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+//       <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
+//       <span class="mdc-top-app-bar__title">Title</span>
+//     </section>
+//   </div>
+// </header>
+//   </div>
+//   );
+// }
+//
+// export default App;
