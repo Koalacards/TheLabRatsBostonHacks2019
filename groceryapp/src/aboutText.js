b6a0329c99024ba29e5192135b0510d3db66f010
jlkazan <jlkazan@gmail.com>
2019-11-17 06:52:21 -0500

    about me

--- groceryapp/src/aboutText.js
+++ groceryapp/src/aboutText.js
@@ -9,10 +9,17 @@ function AboutText()  {
         About Us
       </header>
       <p id='aboutUs'>
-        The Lab Rats are a team of sophomores from Northeastern University who wanted 
-        to make something practical, something we all felt would have contibuted to 
-        our experience. We thought, what conttibutes to experience more than what we 
-        eat? We know the struggle of a broke college student trying to find where.
+        The Lab Rats are a team of sophomores from Northeastern University who wanted
+        to make something practical; something we all felt would have helped make
+        our transition to college easier, along with being able to currently help
+        us for the rest of our years here.We thought, what contributes to experience
+        more than what we eat? We know the struggle of a broke college student trying
+        to find the best deals around, and we also were frustrated with the lack
+        of a single source to compare prices. That's when we decided to create
+        SimpliShop, a platform which actively sweeps the web finding prices from
+        a variety of Boston supermarkets and puts it all in one easy-to-navigate
+        website. Simply type in the item you want, and you will be presented with
+        a list of results from nearby companies so you can can find all the best deals!
       </p>
     </div>
   );
