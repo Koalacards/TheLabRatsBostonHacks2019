1a47eea64f4e32d20d98f53e22bab7b234839236
Ronja <38873366+Rebranding@users.noreply.github.com>
2019-11-16 20:07:17 -0500

    Got a Nav Bar

--- groceryapp/src/Nav.js
+++ groceryapp/src/Nav.js
@@ -1,32 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-function App() {
-  return (
-    <div className="Groceries!">
-      <header className="Find some groceries!" >
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Search for what groceries you want! This website will tell you what the best prices are around!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
-        <br>
-        </br>
-        <input type="text" placeholder="Search for groceries here!">
-
-        </input>
-
-      </header>
+const NavBar = () => {
+  return(
+    <div>
+      <AppBar position = "static">
+        <Toolbar>
+          <Typography variant = "title" color = "inherit">
+          BUY GROCERIES
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default NavBar;
+
+// function App() {
+//   return (
+//     <div className="Groceries!">
+//       <header className="Find some groceries!" >
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Search for what groceries you want! This website will tell you what the best prices are around!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://www.google.com"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Test
+//         </a>
+//         <br>
+//         </br>
+//         <input type="text" placeholder="Search for groceries here!">
+
+//         </input>
+
+//       </header>
+//     </div>
+//   );
+// }
+
+//export default App;
--- groceryapp/src/index.js
+++ groceryapp/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App.js';
 import Nav from './Nav';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Nav />, document.getElementById('root1'));
-ReactDOM.render(<App />, document.getElementById('root2'));
+//ReactDOM.render(<App />, document.getElementById('root2'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
