0c5052cf0d6fe225074ad3db1be3f5c9a7655354
Ronja <38873366+Rebranding@users.noreply.github.com>
2019-11-17 00:21:59 -0500

    Two Exports in Same Module

--- groceryapp/src/Nav.js
+++ groceryapp/src/Nav.js
@@ -3,7 +3,9 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 
-const NavBar = () => {
+
+
+export const NavBar = () => {
   return(
     <div>
       <AppBar position = "static">
@@ -15,9 +17,7 @@ const NavBar = () => {
       </AppBar>
     </div>
   )
-}
-
-export default NavBar;
+};
 
 // function App() {
 //   return (
--- groceryapp/src/index.js
+++ groceryapp/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App.js';
-import Nav from './Nav';
+import {Result} from './Result';
+import {NavBar} from './Nav';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Nav />, document.getElementById('root1'));
-//ReactDOM.render(<App />, document.getElementById('root2'));
+ReactDOM.render(<NavBar />, document.getElementById('NavBar'));
+ReactDOM.render(<Result />, document.getElementById('Result'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
