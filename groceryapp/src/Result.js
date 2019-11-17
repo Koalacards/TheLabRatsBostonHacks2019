349b19a2031475d02fb47df0e3e9202e4bc581c9
Ronja <38873366+Rebranding@users.noreply.github.com>
2019-11-17 00:56:35 -0500

    Renaming

---
+++ groceryapp/src/Result.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+export function Result()  {
+  return (
+    <div>
+      <header>
+        About Us
+      </header>
+      <p>
+        The Lab Rats are a team of sophomores from Northeatern University who wanted 
+        to make something practical, something we all felt would have contibuted to 
+        our experience. We thought, what conttibutes to experience more than what we 
+        eat? We know the struggle of a broke college student trying to find where.
+      </p>
+    </div>
+  );
+};
